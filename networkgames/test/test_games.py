eff77818ade818db4f538a5944fdac65fb6c35e4
Unknown <pfwnicks@gmail.com>
2018-07-24 14:01:18 -0400

    added test directory and added a testfile for core.games
    added to test functions for base.Games and seepage.Seepage

---
+++ networkgames/test/test_games.py
@@ -0,0 +1,18 @@
+def base_games():
+    from networkgames.core.games.base import Games
+    mg = Games()
+    print(mg, mg.player_0, mg.player_1)
+
+base_games()
+
+def seepage_games():
+    import networkx as nx
+    from networkx import generators as gg
+    from networkgames.core.games.seepage import Seepage
+
+    sg = Seepage(gg.caveman_graph(2, 2), 'test', 'test', 'test', 'test')
+
+    print(sg.info, sg.info._fields)
+
+seepage_games()
+
