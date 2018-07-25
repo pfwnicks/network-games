b952f211e9990e3b67c8263783ec4907170eb276
Unknown <pfwnicks@gmail.com>
2018-07-25 10:06:17 -0400

    added printouts for test readouts

--- networkgames/test/test_games.py
+++ networkgames/test/test_games.py
@@ -1,19 +1,23 @@
 def base_games():
     from networkgames.core.games.base import Games
     mg = Games()
-    print(mg, mg.player_0, mg.player_1)
+    print(mg)
+    print(mg.players.player_0._fields, mg.players.player_1)
 
 base_games()
 
 def seepage_games():
-    import networkx as nx
     from networkx import generators as gg
     from networkgames.core.games.seepage import Seepage
 
     sg = Seepage(gg.caveman_graph(2, 2), 'test', 'test', 'test', 'test')
 
-    print(sg.info, sg.info._fields)
+    print(sg.info)
+    print(sg.info._fields)
+    print(sg.info.test_graph)
+    print(sg.players.player_0.name)
+    print(sg.players.player_0.strategy)
 
-seepage_games()
+# seepage_games()
 
 #TODO: Add check functions for all test functions
