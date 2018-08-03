f2048d3b2b56644aea7cc414b853d5b15d31df7a
Unknown <pfwnicks@gmail.com>
2018-08-03 02:37:28 +0200

    refactored startString to updated string manipulation

--- networkgames/core/util/_datalogs.py
+++ networkgames/core/util/_datalogs.py
@@ -1,27 +1,20 @@
+# from networkgames.core.games.base import Games
+from importlib import import_module
+from time import ctime
 output = print
 
-def start_string(cur_game):
-
-    player_cout = "Player Info: \n"
-
-    for player in cur_game.players:
-
-
-    str(
-        '\n Experiment started: ' + str(cur_game.info.experiment_start_time) + '\n\n' +
-        'Testing the seepage problem on the ' + str(g) + ' which has ' +
-        str(cur_game.info.test_graph.order) + ' vertices.\n' + 'The vertices to be tested are: ' +
-        str(cur_game.info.sim_range) + '\n' + 'These vertices will be tested ' +
-        str(cur_game.info.sim_runs) + ' time(s) each with Green using the ' +
-        str(cur_game.info.green_algo) + ' strategy, and Sludge using the ' +
-        str(cur_game.info.sludge_algo) + ' strategy.')
+def start_string(cur_game=import_module('networkgames.core.games.base').Games()):
+    print(cur_game)
+    player_cout = "Player Names and Strategies: \n %s" % \
+        ("".join(["""
+            Name: %s :--: Strategy: %s \n""" % (player.name, player.strategy) for player in cur_game.player_info]))
 
     cout = """\n
     Experiment started: %s \n\n
     Testing the %s on the %s which has %s vertices. \n
     Each vertex in the range %s will be tested %s times. \n
     %s \n
-    """ % (cur_game.info.experiment_start_time, cur_game.name, cur_game.info.test_graph.name,
-           cur_game.info.test_graph.order, cur_game.info.sim_range, cur_game.info.sim_runs, player_cout)
+    """ % (ctime(), cur_game.name, cur_game.sim_info.test_graph.name,
+           cur_game.sim_info.test_graph.order, cur_game.sim_info.sim_range, cur_game.sim_info.sim_runs, player_cout)
 
     return cout
