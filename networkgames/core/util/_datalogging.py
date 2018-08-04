bcb86ff7165f940c4a43720d2ec1d7d27781faca
Unknown <pfwnicks@gmail.com>
2018-08-05 01:38:55 +0200

    renamed _datalogs to _datalogging

--- networkgames/core/util/_datalogging.py
+++ networkgames/core/util/_datalogging.py
@@ -4,6 +4,9 @@ from time import ctime
 output = print
 
 def start_string(cur_game=import_module('networkgames.core.games.base').Games()):
+
+
+
     print(cur_game)
     player_cout = "Player Names and Strategies: \n %s" % \
         ("".join(["""
@@ -18,3 +21,14 @@ def start_string(cur_game=import_module('networkgames.core.games.base').Games())
            cur_game.sim_info.test_graph.order, cur_game.sim_info.sim_range, cur_game.sim_info.sim_runs, player_cout)
 
     return cout
+
+def loc_string(cur_game=import_module('networkgames.core.games.base').Games()):
+    cout = """
+     The directory for this results can be found here: \n %s \n
+     The overall contamination number data for this experiment is in: \n
+     combined_experiment_data.txt\n
+     The directory containing detailed information for the tests on each
+     vertex is: \n %s \n
+     The directory containing the graphs of the maximal seepage
+     situation is: \n %s \n\n
+     """  % (file_path, vertex_file_path, graph_file_path)
