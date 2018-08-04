875198927d5b70aa488b26a5b5429a8c21c7538e
Unknown <pfwnicks@gmail.com>
2018-08-05 01:40:04 +0200

    added data path param and changed rel paths to abs paths

--- networkgames/core/util/_filehandling.py
+++ networkgames/core/util/_filehandling.py
@@ -1,10 +1,11 @@
 import os
+from networkgames.data import results
 
+def create_output_locs(start_time, data_path=os.path.dirname(results.__file__)):
 
-def create_output_directory(start_time):
-    for f_path in ["./data/results/%s/" % start_time,
-                   "./data/results/%s/vertexData/" % start_time,
-                   "./data/results/%s/graphs" % start_time]:
+    for f_path in ["%s/%s/" % (data_path, start_time),
+                   "%s/%s/vertexData/" % (data_path, start_time),
+                   "%s/%s/graphs" % (data_path, start_time)]:
         if not os.path.isdir(f_path):
             os.makedirs(f_path)
     return
