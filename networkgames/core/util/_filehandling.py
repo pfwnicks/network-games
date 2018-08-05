3da95c7d263fe3515d2ba135f25aa49a06ab56c7
Unknown <pfwnicks@gmail.com>
2018-08-05 02:23:46 +0200

    moved start_time retrieval to _filehandling

--- networkgames/core/util/_filehandling.py
+++ networkgames/core/util/_filehandling.py
@@ -1,11 +1,14 @@
 import os
 from networkgames.data import results
 
-def create_output_locs(start_time, data_path=os.path.dirname(results.__file__)):
+def create_output_locs(data_path=os.path.dirname(results.__file__)):
+    from time import clock
+
+    start_time = clock()
 
     for f_path in ["%s/%s/" % (data_path, start_time),
                    "%s/%s/vertexData/" % (data_path, start_time),
                    "%s/%s/graphs" % (data_path, start_time)]:
         if not os.path.isdir(f_path):
             os.makedirs(f_path)
-    return
+    return "%s/%s/" % (data_path, start_time)
